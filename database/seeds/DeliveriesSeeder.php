518cd2e0c613055b77ab89d44d0af218fa7c3f6c
mrivera96 <jylrivera96@gmail.com>
2020-05-17 12:24:15 -0600

    ajustes en los seeders

--- database/seeds/DeliveriesSeeder.php
+++ database/seeds/DeliveriesSeeder.php
@@ -18,7 +18,7 @@ class DeliveriesSeeder extends Seeder
             'nomCliente' => 'Ismael Alexander Gutierrez', 'numIdentificacion' => '07031994389462',
             'numCelular' => '94689230', 'fechaReserva' => \Carbon\Carbon::now(),
             'dirRecogida' => 'Aeropuerto Internacional Toncontín, Tegucigalpa, Col. 15 de Septiembre, Tegucigalpa, Honduras',
-            'email' => 'jylrivera96@gmail.com', 'idCategoria' => 1, 'idEstado' => 33,
+            'email' => 'jylrivera96@gmail.com', 'idCategoria' => 1, 'idEstado' => 36,
             'tarifaBase' => '120.00', 'recargos' => '50.00', 'total' => '170.00',
         ]);
 
@@ -36,10 +36,9 @@ class DeliveriesSeeder extends Seeder
             'nomCliente' => 'Carlos Daniel Almendares', 'numIdentificacion' => '0801199001632',
             'numCelular' => '33946384', 'fechaReserva' => \Carbon\Carbon::now(),
             'dirRecogida' => 'Centro Comercial Los Castaños, Boulevard Morazán, Tegucigalpa, Honduras',
-            'email' => 'jylrivera96@gmail.com', 'idCategoria' => 3, 'idEstado' => 33,
-            'tarifaBase' => '145.00', 'recargos' => '50.00', 'total' => '195.00', 'isPagada' => 1
+            'email' => 'jylrivera96@gmail.com', 'idCategoria' => 3, 'idEstado' => 36,
+            'tarifaBase' => '145.00', 'recargos' => '50.00', 'total' => '195.00'
         ]);
-        \Illuminate\Support\Facades\DB::unprepared('SET IDENTITY_INSERT tblDeliveries OFF');
 
 
         \Illuminate\Support\Facades\DB::table('tblDetalleDelivery')->insert([
@@ -50,5 +49,45 @@ class DeliveriesSeeder extends Seeder
             'direccion' => 'Residencial Honduras, Tegucigalpa',
             'distancia' => '10.5 Km'
         ]);
+
+        \Illuminate\Support\Facades\DB::table('tblDeliveries')->insert([
+            'idDelivery' => 4,
+            'nomCliente' => 'Rony Pavon', 'numIdentificacion' => '0801199300151',
+            'numCelular' => '9933-0807', 'fechaReserva' => '2020-05-15 19:47:00.000',
+            'dirRecogida' => 'Ecovivienda Villa Olímpica, Tegucigalpa, Honduras',
+            'email' => 'rony_pavon@hotmail.com', 'idCategoria' => 1, 'idEstado' => 36,
+            'tarifaBase' => '120.00', 'recargos' => '150.00', 'total' => '270.00',
+        ]);
+
+
+        \Illuminate\Support\Facades\DB::table('tblDetalleDelivery')->insert([
+            'idDelivery' => 4,
+            'nFactura' => '0001',
+            'nomDestinatario' => 'Alejandra Pavon',
+            'numCel' => '9902-3767',
+            'direccion' => 'Valle de Ángeles, Honduras',
+            'distancia' => '26.2 km'
+        ]);
+
+        \Illuminate\Support\Facades\DB::table('tblDeliveries')->insert([
+            'idDelivery' => 5,
+            'nomCliente' => 'Fernando Ramos', 'numIdentificacion' => '0801198613914',
+            'numCelular' => '3395-4713', 'fechaReserva' => '2020-05-15 19:46:00.000',
+            'dirRecogida' => 'Valle de Ángeles, Honduras',
+            'email' => 'josefferhn@gmail.com', 'idCategoria' => 1, 'idEstado' => 36,
+            'tarifaBase' => '120.00', 'recargos' => '400.00', 'total' => '520.00',
+        ]);
+
+
+        \Illuminate\Support\Facades\DB::table('tblDetalleDelivery')->insert([
+            'idDelivery' => 5,
+            'nFactura' => '123',
+            'nomDestinatario' => 'Rony',
+            'numCel' => '3395-4713',
+            'direccion' => 'Plaza Marie, Tegucigalpa, Honduras',
+            'distancia' => '30.2 km'
+        ]);
+
+        \Illuminate\Support\Facades\DB::unprepared('SET IDENTITY_INSERT tblDeliveries OFF');
     }
 }
