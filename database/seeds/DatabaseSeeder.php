1cefc12637b8706d2276f087f03a3577b1441f45
mrivera96 <jylrivera96@gmail.com>
2020-06-02 21:43:52 -0600

    actualizaciones en seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(DeliveryCategoriesSeeder::class);
         $this->call(clsTarifasDelivery::class);
-        //$this->call(tblClientesDeliverySeeder::class);
+        $this->call(tblClientesDeliverySeeder::class);
         $this->call(SucursalesClientesDelivery::class);
-        //$this->call(UsuariosDeliverySeeder::class);
-        $this->call(DeliveriesSeeder::class);
+        $this->call(UsuariosDeliverySeeder::class);
+        //$this->call(DeliveriesSeeder::class);
 
     }
 }
