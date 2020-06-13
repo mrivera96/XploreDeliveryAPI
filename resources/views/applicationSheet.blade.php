d5abadb4b8ad3fbb5f68b67e630b1edac02277bf
mrivera96 <jylrivera96@gmail.com>
2020-06-13 17:34:12 -0600

    cambio para testeo REVERTIR

--- resources/views/applicationSheet.blade.php
+++ resources/views/applicationSheet.blade.php
@@ -30,7 +30,7 @@
 <body>
 
 <section>
-    <img src="http://190.4.56.14/XploreDeliveryAPI/img/logo-delivery.png"
+    <img src="http://190.4.56.14/XploreDeliveryAPIDesa/img/LOGO-XPLORE_01-ColorPrimario.png"
          style="width: 20%; margin-left: 0">
     <p style="line-height: 0.8">Es un placer saludarte, <strong>{{$delivery->nomCliente}}</strong></p>
     <p style="line-height: 0.8">Hemos recibido tu solicitud de Delivery, y estamos para ayudarte. A continuación
