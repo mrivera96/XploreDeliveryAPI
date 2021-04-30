4db3877457b98d0ad594bf13dc6ad7a3211269a8
mrivera96 <jylrivera96@gmail.com>
2021-04-30 00:56:15 -0600

    formatear fecha desde api

--- app/Http/Controllers/DeliveryUsersController.php
+++ app/Http/Controllers/DeliveryUsersController.php
@@ -319,7 +319,7 @@ class DeliveryUsersController extends Controller
 
 
             foreach ($pendingOrders as $order){
-                $order->delivery->fechaReserva = \Carbon\Carbon::parse($order->delivery->fechaReserva)->format('yyyy-MM-dd h:i a');
+                $order->delivery->fechaReserva = \Carbon\Carbon::parse($order->delivery->fechaReserva)->format('Y-m-d h:i a');
             }
 
             return response()->json([
